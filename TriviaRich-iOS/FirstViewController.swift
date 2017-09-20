ade0b788dc07a212a90d9ce63c04ba0a6914ab55
Matthew Rigdon <rigdonmr@gmail.com>
2017-09-19 23:50:28 -0700

    Initial Commit

---
+++ TriviaRich-iOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TriviaRich-iOS
+//
+//  Created by Matthew Rigdon on 9/19/17.
+//  Copyright © 2017 com.rigdon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
